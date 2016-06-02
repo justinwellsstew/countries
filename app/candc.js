4b395bdc15160cde2d40bbd290d3ed186c680008
Justin Stewart <stewartjustin3@gmail.com>
2016-06-02 11:59:19 -0400

    added capital pop and neighbor countries to factories

--- app/candc.js
+++ app/candc.js
@@ -11,23 +11,28 @@ angular.module('CACApp', ['ngRoute'])
         	controller : 'DetailCtrl'
         }).otherwise('/')
     }])
+    .factory("countryData", function($http) {
+        return function() {
+            return $http.get('http://api.geonames.org/countryInfoJSON?username=stewartj')
+        };
+    })
+    .factory("capitalPopulationData", function($http) {
+        return function(capital, code) {
+            return $http.get('http://api.geonames.org/searchJSON?name_equals='+capital+'&country='+code+'&username=stewartj')
+        };
+    })
+    .factory("countryNeighborData", function($http) {
+        return function(geonameId) {
+            return $http.get('http://api.geonames.org/neighboursJSON?geonameId='+geonameId+'&username=stewartj')
+        };
+    })
     .controller('HomeCtrl', function($scope) {
         //empty for now
     })
-    .controller('CountriesCtrl', function($scope, $rootScope, $cacheFactory, $http){
-        var url = 'http://api.geonames.org/countryInfoJSON?username=stewartj';
-
-    	$http.get(url,{
-            cache : true
-        })
+    .controller('CountriesCtrl', function($scope, $rootScope, countryData, $http){
+    	countryData()
     	.then(function(data){
     		$scope.countryNames = data.data.geonames;
-            $rootScope.countryNames = $scope.countryNames;
-
-            //caching
-            // var cache = $cacheFactory.get('$http');
-            // var cachedResponse = cache.get(url); 
-            // console.log(cachedResponse);
     	},
     		function(){
     		console.log('fail')	
@@ -35,45 +40,51 @@ angular.module('CACApp', ['ngRoute'])
     	);
 
     })
-    .controller('DetailCtrl', function($scope, $rootScope, $http, $routeParams){
-        // var url = 'http://api.geonames.org/countryInfoJSON?username=stewartj';
-
-        // get country from country page 
+    .controller('DetailCtrl', function($scope, $http, $routeParams, countryData, capitalPopulationData, countryNeighborData){
+ 
+        // get ID from country page 
     	$scope.selectedCountry = $routeParams.countryCode;
 
     
-        //get country data from country page
-        $scope.countryNames = $rootScope.countryNames;
-
-        angular.forEach($scope.countryNames, function(value, key) {
-           if($scope.selectedCountry == value.fipsCode){
-            $scope.population = value.population;
-            $scope.area = value.areaInSqKm;
-            $scope.capital = value.capital;
-            $scope.geonameId = value.geonameId;
-           }
-           console.log (value.fipsCode);
-        });
-
-        console.log($scope.countryNames);
-
-    	$http.get('http://api.geonames.org/searchJSON?name_equals='+$scope.capital+'&country='+$scope.selectedCountry+'&username=stewartj')
-    	.then(function(data){
-            if(data.data.geonames[0]['population']){
-    		$scope.capitalPopulation = data.data.geonames[0]['population'];
+        //get country data from factory
+        countryData()
+        .then(function(data){
+            // $scope.countryNames = data.data.geonames;
+            $scope.countryName = data.data.geonames[$scope.selectedCountry].countryName;
+            $scope.population = data.data.geonames[$scope.selectedCountry].population;
+            $scope.countryCode = data.data.geonames[$scope.selectedCountry].fipsCode;
+            $scope.area = data.data.geonames[$scope.selectedCountry].areaInSqKm;
+            $scope.capital = data.data.geonames[$scope.selectedCountry].capital;
+            $scope.geonameId = data.data.geonames[$scope.selectedCountry].geonameId;
+            return capitalPopulationData($scope.capital, $scope.countryCode);
+        },
+            function(){
+            console.log('fail') 
             }
-    	},function(){
-    		console.log('something went wrong');
-    	});
+        )
+
+        .then(function(data) {
+            console.log(data);
+      //  Store the profile, now get the permissions.
+        if(data.data.geonames[0]['population']){
+        $scope.capitalPopulation = data.data.geonames[0]['population'];
+        }else{
+            $scope.capitalPopulation = "unknown";
+        }
+        return countryNeighborData($scope.geonameId);
+        }, 
+
+        function(){
+            console.log('Something went wrong')
+        })
 
 
-        $http.get('http://api.geonames.org/neighboursJSON?geonameId='+$scope.geonameId+'&username=stewartj')
         .then(function(data){
             var neighbors = "";
            angular.forEach(data.data.geonames, function(value, key) {
             neighbors = neighbors + value.name + " ";
-        });
-        $scope.neighbors = neighbors;   
+            });
+            $scope.neighbors = neighbors;   
         }, function(){
             console.log('something went wrong');
             })
